20e09e1bea57e6a535bef204b21fd4cb8c25994e
Yash Bhatt <yash.bhatt@milliman.com>
2021-07-04 14:57:26 +0530

    Data loaded

--- master.R
+++ master.R
@@ -1,1 +1,7 @@
-#Cluster analysis
+#Libraries:
+library(dplyr)
+library(ggplot2)
+
+#loading the datasets:
+salaries <- read.csv("Salaries1.csv", header = TRUE, sep = ",")
+season_stats <- read.csv("Seasons_Stats.csv", header = TRUE, sep = ",")
