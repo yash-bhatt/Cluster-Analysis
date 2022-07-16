89c2b9ee36ec8dc78138848e74ce0ed0278f6fa5
Yash Bhatt <c-yash.bhatt@milliman.com>
2022-07-17 01:41:54 +0530

    Saved plots

--- master.R
+++ master.R
@@ -1,6 +1,12 @@
 #Libraries:
 library(dplyr)
+library(tidyr)
 library(ggplot2)
+library(data.table)
+library(tibble)
+library(ggcorrplot)
+library(GGally)
+
 
 #loading the datasets:
 salaries <- read.csv("Salaries1.csv", header = TRUE, sep = ",")
@@ -19,7 +25,7 @@ nba <- season_stats %>%
   select(c("Year", "Player", "Tm", "Pos", "Age", "G","MP","FG", "FGA", "3P",
            "3PA", "2P", "2PA","FT", "FTA", "TRB", "AST", "STL", "BLK", "PTS", "VORP", "PER"))
 
-#Normalize on a per game basis for better comparions 
+#Normalize on a per game basis for better comparison
 nba_per_game <- data.frame(sapply(nba %>% select(c("MP", "FG", "FGA", "3P", "3PA", "2P",
                                          "2PA", "FT", "FTA", "TRB", "AST", "STL","BLK", "PTS")),
                                   function(x){x/nba$G}))
@@ -28,36 +34,35 @@ colnames(nba_per_game) = paste0(c("MP", "FG", "FGA", "3P", "3PA", "2P","2PA", "F
                                   "AST", "STL","BLK", "PTS"), "_pg")
 nba <- cbind(nba, nba_per_game)
 
-#Players who switched teams (duplicate observations issue)
+## Handling duplicate observations issue
+# Players who switched teams 
 number_teams <- data.frame(nba %>% count(Player) %>% arrange(desc(n)) %>% rename("number_teams" = "n"))
 nba <- left_join(nba, number_teams, by = "Player")
 players_multiple_teams <- nba %>% filter(number_teams >1)
-players_minutes_max <- data.frame(players_multiple_teams %>% filter(Tm != "TOT") %>% select(Player, MP) %>% group_by(Player) %>% summarize(max_min = max(MP))) #Why we are not taking ToT? Tm stands for team.
-team_minutes_max <- data.frame(left_join(players_minutes_max, players_multiple_teams[,c("Player", "MP", "Tm")], by = c("max_min" = "MP", "Player")))
+players_minutes_max <- data.frame(players_multiple_teams %>% filter(Tm != "TOT") %>% select(Player, MP) %>% group_by(Player) %>% summarize(max_min = max(MP))) 
+team_minutes_max <- data.frame(left_join(players_minutes_max, players_multiple_teams %>% select(c("Player", "MP", "Tm")), by = c("max_min" = "MP", "Player")))
 player_team_assign <- data.frame(left_join(players_multiple_teams %>% select(Player, Tm) %>% filter(Tm == "TOT"), team_minutes_max %>% select(Player, Tm), by = "Player"))
 players_one_team <- data.frame(nba %>% filter(number_teams == 1) %>% select(Player, Tm))
 players_one_team$copy <- players_one_team$Tm
 
-library(data.table)
 
 setnames(players_one_team, old = names(players_one_team), new = names(player_team_assign))
 final_teams <- rbind(player_team_assign, players_one_team)
 nba <- left_join(nba, final_teams, by = c("Player", "Tm" = "Tm.x"))
 nba <- nba[, -which(names(nba) %in% c("Tm", "number_teams"))]
 
-library(tibble)
 
 nba <- add_column(nba, nba$Tm.y, .after = "Player")
 colnames(nba)[3] <- "Tm"
 
 sapply(nba, function(x) sum(is.na(x)))
-nba <- nba[!is.na(nba$Tm), -which(names(nba) %in% "Tm.y")]
+nba <- nba %>% filter(!is.na(nba$Tm)) %>% select(-which(names(nba) %in% "Tm.y"))
 
 #Check for missing values 
 sapply(nba, function(x) sum(is.na(x)))
 
 #Salaries dataset
-## Exploring the dataset
+## Explore the dataset
 names(salaries)
 str(salaries)
 colSums(is.na(salaries))
@@ -68,11 +73,9 @@ finalnba <- left_join(nba, salaries, by = c("Player" = "NAME"))
 
 str(finalnba)
 
+#######################################################################
+
 #EDA on the final dataset:
-library(tidyr)
-library(ggplot2)
-library(GGally)
-library(corrplot)
 
 #Density plot
 features <- grep("_pg", names(finalnba), value = TRUE)
@@ -86,12 +89,18 @@ features %>%
   facet_wrap(~name, scales = "free") + 
   labs(title = "Density Plot", x= "")
 
+ggsave('feature_distribution..png',
+       plot = last_plot(),
+       scale = 5)
+
 #All plots are looking positively skewed except for the Minutes played
 
 # corr plot
 a <- cor(features)
-corrplot(a, type = "lower")
+ggcorrplot(a, type = "lower", title = 'Correlation between features') 
 
+ggsave('feature_corr.png',
+       scale = 5)
 # All seems to have a positive corr.
 
 #PCA time
@@ -113,6 +122,7 @@ summary(pca)
 table(finalnba$Pos, useNA = "always")
 
 autoplot(pca, data = finalnba, colour = "Pos")
+
 scree(scaled_features)
 
 #Modelling
